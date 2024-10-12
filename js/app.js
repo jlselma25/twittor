73a97fa14a2ea6f0a12bd31b0d16099ede9be566
Jose Selma Hoyo <jlselma25@gmail.com>
2024-10-12 09:55:30 +0200

    Nodificacion ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+
+var url = window.location.href;
+var locationUrl ='/twittor/sw.js';
+
+
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')){
+        locationUrl='/sw.js';        
+    }
+    
+    navigator.serviceWorker.register(locationUrl);
+
+    
 }
 
 
