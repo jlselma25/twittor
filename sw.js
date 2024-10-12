21b3786bdbea942fcec038d1fc3381876bc05c01
Jose Selma Hoyo <jlselma25@gmail.com>
2024-10-12 17:18:03 +0200

    Eliminar version 2

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE ='static-v3';
-const DYNAMIC_CACHE ='dynamic-v3';
+const STATIC_CACHE ='static-v2';
+const DYNAMIC_CACHE ='dynamic-v1';
 const INMUTABLE_CACHE ='inmutable-v1';
 
 const APP_SHELL = [
@@ -52,8 +52,8 @@ self.addEventListener('activate', e=>{
             if(key != STATIC_CACHE && key.includes('static'))
                 return caches.delete(key);
 
-            if(key != DYNAMIC_CACHE && key.includes('dynamic'))
-                return caches.delete(key);
+            // if(key != DYNAMIC_CACHE && key.includes('dynamic'))
+            //     return caches.delete(key);
         });
 
     });
